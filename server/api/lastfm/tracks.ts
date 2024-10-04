6930b3ca84e2a9dce9402fa0b56247a4768cef7f
Pedrvisk <pedrobrbrpedro@gmail.com>
2024-10-04 10:09:15 -0300

    Endpoint para retornar as músicas do LastFM, feito bypass nas imagens.

---
+++ server/api/lastfm/tracks.ts
@@ -0,0 +1,79 @@
+import { JSDOM } from "jsdom";
+
+interface LastFMTrackArtist {
+  url: string;
+  name: string;
+}
+
+interface LastFMTrackImage {
+  "#text": string;
+  size: string;
+}
+
+export interface LastFMTrack {
+  name: string;
+  artist: LastFMTrackArtist;
+  url: string;
+  rank: string;
+  playcount: string;
+  image: string | null;
+}
+
+interface LastFMTrackResponse {
+  toptracks: {
+    track: (LastFMTrack & {
+      "@attr": { rank: string };
+      image: LastFMTrackImage[];
+    })[];
+  };
+}
+
+async function fetchArtistImages(artist: string): Promise<string[]> {
+  const url = `http://www.last.fm/fr/music/${artist}/+images`;
+  const html = await fetch(url).then((response) => response.text());
+
+  const imgs: string[] = [];
+  const dom = new JSDOM(html);
+  const elements = dom.window.document.getElementsByTagName("img");
+
+  for (const element of elements) {
+    const src = element.getAttribute("src");
+    if (src) imgs.push(src);
+  }
+
+  return imgs;
+}
+
+export default defineEventHandler(
+  async (event): Promise<{ data: LastFMTrack[] | null }> => {
+    const res = await $fetch<LastFMTrackResponse>(
+      `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=Pedrov1sk&api_key=${process.env.LASTFM_API_KEY}&format=json`
+    ).catch(() => null);
+
+    if (!res || !res.toptracks || res.toptracks.track.length === 0) {
+      return { data: null };
+    }
+
+    const tracks = await Promise.all(
+      res.toptracks.track.map(async (track) => {
+        const artistImages = await fetchArtistImages(track.artist.name);
+
+        return {
+          name: track.name,
+          artist: {
+            url: track.artist.url,
+            name: track.artist.name,
+          },
+          url: track.url,
+          rank: track["@attr"].rank,
+          playcount: track.playcount,
+          image: artistImages[1] || null,
+        };
+      })
+    );
+
+    return {
+      data: tracks,
+    };
+  }
+);
